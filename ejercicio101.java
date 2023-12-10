d19de55c7e9a90bc7c2b47a95654971e39f4d814
CarJaverFer <carlosfer2001@protonmail.com>
2023-12-10 19:03:53 +0100

    first commit

---
+++ ejercicio101.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+/**
+ * @author crsfz (Carlos Javier Ferrer Zapata)
+ * Algoritmo que permita obtener el factorial de un número dado
+ */
+
+public class ejercicio101 {
+    
+    public static void main(String []args){
+
+        Scanner entrada = new Scanner (System.in);
+        System.out.println("ingrese numero");
+        int x = entrada.nextInt();
+        int fact=1; int i=0;
+        for (i=1;i<x+1;i++){
+            fact=fact*i;
+        }
+        System.out.println("el factorial del nº es: "+fact);
+
+    }
+}
